03d2ba6b8f993ce48c939756609699a04d974fc0
omodolapoolabanji <134759838+omodolapoolabanji@users.noreply.github.com>
2024-04-14 16:50:55 -0400

    new cmmit

---
+++ src/main/java/edu/iu/bolabanj/c322spring2024homework2/security/Jwks.java
@@ -0,0 +1,23 @@
+package edu.iu.habahram.ducksservice.security;
+
+
+import java.security.interfaces.RSAPublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.KeyPair;
+import java.util.UUID;
+import com.nimbusds.jose.jwk.RSAKey;
+
+
+public class Jwks {
+    private Jwks(){}
+    public static RSAKey generateRSA(){
+        KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey  = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+}
